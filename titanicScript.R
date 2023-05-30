dbd185f4e228c0b507fcf7b4ef669174374ff1a1
unknown <vitorjoaocomp1997@gmail.com>
2023-05-29 22:19:21 -0300

    wranglingii

--- titanicScript.R
+++ titanicScript.R
@@ -1,8 +1,11 @@
 install.packages("tidyverse")
 install.packages("readxl")
 install.packages("knitr")
+install.packages("caret")
 
 library("tidyverse")
+library("caret")
+library("dplyr")
 
 gender_submission <- read.csv("gender_submission.csv")
 test <- read.csv("test.csv")
@@ -15,4 +18,24 @@ varaveis_train <-c("idp", "survived", "classp", "name", "sex", "age", "sib", "pa
 names(gender_submission) <- variaveis_gs
 names(test) <- varaveis_teste
 names(train) <- varaveis_train
+
+mean_age <- mean(train$age, na.rm = TRUE)
+mean_test <- mean(test$age, na.rm = TRUE)
+mean_fare <- mean(test$fare, na.rm = TRUE)
+
+train$age[is.na(train$age)] <- mean_age
+test$age[is.na(test$age)] <- mean_test
+train$fare[is.na(train$fare)] <- mean_fare
+test$fare[is.na(test$fare)] <- mean_fare
+
+train$age <- round(train$age)
+test$age <- round(test$age)
+
+train$sex <- ifelse(train$sex == "male", 0, 1)
+test$sex <- ifelse(test$sex == "male", 0, 1)
+
+
+
+
+
 
